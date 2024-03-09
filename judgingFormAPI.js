2ba499d9cdc3a908a0e5712240c7fb8f0a5c0ec8
VidsterBroyoMinvest <63118171+VidsterBroyo@users.noreply.github.com>
2024-03-09 12:44:25 +0530

    backend setup

--- judgingFormAPI.js
+++ judgingFormAPI.js
@@ -15,8 +15,18 @@ app.use("/static", express.static('./static/'))
 const PORT = process.env.PORT || 5000
 
 
-// home page
+// login page
 app.get('/', async (req, res) => {
+    // read from index.html file
+    let data = await fsP.readFile('./login.html')
+
+    // send html
+    res.writeHead(200, { 'Content-Type': 'text/html' }).end(data)
+})
+
+
+// home page
+app.get('/rubrics', async (req, res) => {
     // read from index.html file
     let data = await fsP.readFile('./index.html')
 
@@ -25,20 +35,20 @@ app.get('/', async (req, res) => {
 })
 
 
-// get request
-app.get('/getRubric/:judge/:teamName', async (req, res) => {
+// get all judge's rubrics
+app.get('/getRubrics/:judge/', async (req, res) => {
 
     var judge = req.params.judge
     var teamName = req.params.teamName
 
     try {
         // get rubric data
-        let data = JSON.parse(await fsP.readFile('./rubrics.json'))[0].rubrics
-        console.log(data[judge][teamName])
+        let rubrics = JSON.parse(await fsP.readFile('./rubrics.json'))
+        console.log(rubrics[judge])
 
         // send json
         res.setHeader('content-type', 'application/json')
-        res.json(data[judge][teamName])
+        res.json(rubrics[judge])
         res.end()
 
     } catch (error) {
@@ -47,26 +57,60 @@ app.get('/getRubric/:judge/:teamName', async (req, res) => {
 })
 
 
-// updating rubric
-app.put('/putRubric/:judge/:teamName', async (req, res) => {
+// get judge's rubric for 1 team
+app.get('/getRubric/:judge/:teamName', async (req, res) => {
 
     var judge = req.params.judge
-    var teamName = req.params.teamName
+    var requestedTeamName = req.params.teamName
 
-    let data = JSON.parse(await fsP.readFile('./rubrics.json'))
-
-    pretendRequestJSON = { 'Criterion A': { Score: -3, Comments: 'terrible' } }
-    // when the frontend is good the just use xmlrequest to send json info here
-
-    data[0].rubrics[judge][teamName] = pretendRequestJSON
-    
     try {
-        await fsP.writeFile('./rubrics.json', JSON.stringify(data))
+        // get rubric data
+        let rubrics = JSON.parse(await fsP.readFile('./rubrics.json'))[judge]
+        console.log(rubrics)
+        
+        // find the right rubric
+        for (i=0; i<rubrics.length; i++){
+            if (rubrics[i]["TeamName"] == requestedTeamName){
+                rubric = rubrics[i]
+                break
+            }
+        }
+
+        // send json
+        res.setHeader('content-type', 'application/json')
+        res.json(rubric)
+        res.end()
+
+    } catch (error) {
+        res.end("getRubric response error: " + error)
+    }
+})
+
+
+// updating a rubric
+app.put('/updateRubric/:judge/:teamName', async (req, res) => {
+    try {
+        var judge = req.params.judge
+        var teamName = req.params.teamName
+
+        let rubrics = JSON.parse(await fsP.readFile('./rubrics.json'))
+
+        // find the right rubric
+        for (i=0; i<rubrics[judge].length; i++){
+            if (rubrics[judge][i]["TeamName"] == teamName){
+                rubrics[judge][i] = req.body
+                break
+            }
+        }
+
+        console.log(rubrics)
+    
+        await fsP.writeFile('./rubrics.json', JSON.stringify(rubrics))
         res.end("success")
     }
     catch (error) {
         console.error(error)
-        res.end("putRubric response error: " + error)
+        res.end("postRubric response error: " + error)
     }
     
 })
---
+++ static/judgingForm.js
@@ -0,0 +1,129 @@
+judges = ["Michael", "Trevor", "Franklin"]
+judge = ""
+team = ""
+
+function login() {
+    user = document.getElementById('user').value
+    password = document.getElementById('password').value
+
+
+    if (judges.includes(user) && password == "Cup2024r0cks!") {
+        sessionStorage.setItem('judge', user)
+
+        alert("Login success")
+        window.location.href = "http://localhost:5000/rubrics"
+
+    } else {
+        alert("Invalid credentials")
+    }
+}
+
+
+function getJudge() {
+    if (sessionStorage.getItem('judge')){
+        judge = sessionStorage.getItem('judge')
+        document.getElementById("welcome").innerText = "Welcome, " + judge
+    } else {
+        alert("You need to log in again")
+        window.location.href = "http://localhost:5000"
+    }
+
+}
+
+
+function loadRubrics() {
+    getJudge() 
+
+    console.log("running")
+    try {
+        var request = new XMLHttpRequest()
+        request.open('GET', `http://localhost:5000/getRubrics/${judge}`, false)
+        request.send()
+
+        rubrics = JSON.parse(request.responseText)
+        console.log(rubrics)
+
+        for (i=0; i<rubrics.length; i++){
+            document.getElementById("rubricsTable").innerHTML += `<tr>
+                                                                      <th scope="row">${rubrics[i]["TeamName"]}</th>
+                                                                      <td><button onclick="displayRubric('${rubrics[i]["TeamName"]}')">Open Rubric</button></td>
+                                                                  </tr>`
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+function displayRubric(teamName) {
+    try {
+        team = teamName
+
+        var request = new XMLHttpRequest()
+        request.open('GET', `http://localhost:5000/getRubric/${judge}/${teamName}`, false)
+        request.send()
+
+        rubric = JSON.parse(request.responseText)
+        console.log(rubric)
+        console.log(rubric["Criterion A"])
+
+
+        document.getElementById("rubricTitle").innerHTML = teamName
+
+        document.getElementById("CAS").value = rubric["Criterion A"]["Score"]
+        document.getElementById("CAC").value = rubric["Criterion A"]["Comments"]
+
+        document.getElementById("CBS").value = rubric["Criterion B"]["Score"]
+        document.getElementById("CBC").value = rubric["Criterion B"]["Comments"]
+
+        document.getElementById("CCS").value = rubric["Criterion C"]["Score"]
+        document.getElementById("CCC").value = rubric["Criterion C"]["Comments"]
+
+        document.getElementById("rubric").style.display = "block"
+        document.getElementById("allRubrics").style.display = "none"
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+function saveRubric() {
+    try {
+
+
+        newRubric = {
+            "TeamName":team,
+            "Criterion A": {"Score":document.getElementById("CAS").value, "Comments":document.getElementById("CAC").value},
+            "Criterion B": {"Score":document.getElementById("CBS").value, "Comments":document.getElementById("CBC").value},
+            "Criterion C": {"Score":document.getElementById("CCS").value, "Comments":document.getElementById("CCC").value}
+        }
+
+
+
+        var request = new XMLHttpRequest()
+        request.open('PUT', `http://localhost:5000/updateRubric/${judge}/${team}`, false)
+        request.setRequestHeader("Content-type", "application/json");
+        request.send(JSON.stringify(newRubric))
+
+
+
+        alert("Saved successfully!")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+function back(){
+    document.getElementById("rubric").style.display = "none"
+    document.getElementById("allRubrics").style.display = "block"
+}
+
+function signOut(){
+    sessionStorage.removeItem('judge')
+    judge = ""
+    window.location.href = "http://localhost:5000/";
+}
